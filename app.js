5e0503758ba7dc4ef1b60f33794a6f9a2bac6133
Rosana Moreira <rooh.moreiraa@gmail.com>
2021-09-14 19:11:42 -0300

    manipulaçao de cor pronta

--- app.js
+++ app.js
@@ -1,7 +1,38 @@
+const container = document.querySelector(".container");
+const listaAtividades = document.querySelector(".lista_atividades");
+const input = document.querySelector(".input");
+const erro = document.querySelector(".erro");
+const botaoCadastra = document.querySelector(".botao_adc");
+const paleta1 = document.querySelector("#paleta1");
+const paleta2 = document.querySelector("#paleta2");
+const paleta3 = document.querySelector("#paleta3");
+
+function criarAtividade(){
+    const atividade = document.createElement("div");
+    atividade.classList.add("atividade");
+    const nomeAtividade = document.createElement("p");
+    atividade.textContent = input.value;
+    const botaoLimpar = document.createElement("button");
+    botaoLimpar.textContent = "limpar";
+    botaoLimpar.classList.add("botao_del");
+    listaAtividades.appendChild(atividade);
+    atividade.appendChild(nomeAtividade);
+    atividade.appendChild(botaoLimpar);
+}
+function definePaleta(cor){
+    container.style.background = cor;
+    listaAtividades.style.background = cor;
+}
+function removeAtividades(){
+    while(listaAtividades.firstElementChild){
+        listaAtividades.removeChild(listaAtividades.firstElementChild);
+    }
+}
+
 function cadastraAtividade(){
     if(input.value.length > 3){
         erro.style.display = "none";
-        //criaAtividade();
+        criarAtividade();
     }else{
         erro.style.display = "grid";
         erro.innerHTML = `${input.value} não é uma atividade válida!`
